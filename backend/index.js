36aee9e21ac903fd0b71cc0fd8bc60073ac84ede
Samind2 <suphaneerangsirat@gmail.com>
2024-09-12 16:11:46 +0700

    cors

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,9 @@ const financialRouter = require("./router/financial.router")
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
+const frontend_url=process.env.FRONTEND_URL
 const corsOption = {
-    origin:"http://localhost:5173"
+  origin:frontend_url
 }
 
 
